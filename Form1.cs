bbfff9c1334e051fbbead476cbbad145a9e9a4ae
Titan <titanxiiplays@gmail.com>
2026-06-17 11:01:32 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+namespace GreetingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter your name.");
+            }
+            else
+            {
+                MessageBox.Show("Hello, " + textBox1.Text + "!");
+            }
+        }
+    }
+}
